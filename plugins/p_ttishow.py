8763923fc310b5811938a93752d3597da889702e
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2023-03-31 12:04:58 +0400

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -64,7 +64,7 @@ async def save_group(bot, message):
                                                                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                                                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                                                                         ],[
-                                                                           InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
+                                                                           InlineKeyboardButton("📥 Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ 📥", url="t.me/Movies_Stream")
                                                                          ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
